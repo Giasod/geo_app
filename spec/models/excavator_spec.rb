1d0a9c0fbe7e1d3ed640f9559ad209a22c12178a
Amiran_Tsiklauri <amiranlevanovich@gmail.com>
2023-07-10 14:01:18 +0300

    add specs for model, add shoulda matchers

---
+++ spec/models/excavator_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Excavator, type: :model do
+  it { should belong_to(:ticket) }
+
+  it { should validate_presence_of(:company_name) }
+end
---
+++ spec/models/ticket_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Ticket, type: :model do
+  it { should have_many(:excavators) }
+
+  it { should validate_presence_of(:well_known_text) }
+end
